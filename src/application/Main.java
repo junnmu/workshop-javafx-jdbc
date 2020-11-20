3ea468d99a2392f9949e5fd4fce9df2f8d3b6c73
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2020-11-20 01:33:17 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,14 +11,18 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
+
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
-            Scene mainScene = new Scene(scrollPane);
+
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
@@ -26,6 +30,10 @@ public class Main extends Application {
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
