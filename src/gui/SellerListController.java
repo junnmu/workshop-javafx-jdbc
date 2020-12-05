6e226fe7e1ea9c122e5165ce7ae6fd076e2519ba
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2020-12-05 20:10:37 -0300

    Department ComboBox

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -19,6 +19,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
+import model.services.DepartmentService;
 import model.services.SellerService;
 
 import java.io.IOException;
@@ -114,7 +115,8 @@ public class SellerListController implements Initializable, DataChangeListener {
             // e atualizando os dados do formulário
             SellerFormController controller = loader.getController();
             controller.setSeller(obj);
-            controller.setSellerService(new SellerService());
+            controller.setServices(new SellerService(), new DepartmentService());
+            controller.loadAssociatedObjects();
             controller.subscribeDataChangeListener(this);
             controller.updateFormData();
 
@@ -127,6 +129,7 @@ public class SellerListController implements Initializable, DataChangeListener {
             dialogStage.initModality(Modality.WINDOW_MODAL);
             dialogStage.showAndWait();
         } catch (IOException e) {
+            e.printStackTrace();
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
     }
